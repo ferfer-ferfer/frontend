c15b728d97fd119ae183c398d77d41f36fb3f5eb
ferfer-ferfer <ferielmenouerinformatique@gmail.com>
2025-05-26 23:30:01 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,11 @@
+// if no token dont acces 
+document.addEventListener('DOMContentLoaded', () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        // Not logged in, redirect to login page
+        window.location.href = '/index.html';
+    }
+});
 
 
 
